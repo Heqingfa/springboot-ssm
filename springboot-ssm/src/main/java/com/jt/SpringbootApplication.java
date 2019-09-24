6375b382c6fe04633a4a3b2516f4f545b5ba3c93
36121 <36121@DESKTOP-5FFLAFB>
2019-09-24 17:31:57 +0800

    e

--- springboot-ssm/src/main/java/com/jt/SpringbootApplication.java
+++ springboot-ssm/src/main/java/com/jt/SpringbootApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Springboot1Application {
+public class SpringbootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Springboot1Application.class, args);
+		SpringApplication.run(SpringbootApplication.class, args);
 	}
 
 }
